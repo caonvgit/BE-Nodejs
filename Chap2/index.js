24aca098c5ce6e861f2a50e3c22bc3d63c7a8b77
[cao nguyen] <[caonguyengithub@gmail.com-email]>
2024-08-20 21:17:56 +0700

    router

--- Chap2/index.js
+++ Chap2/index.js
@@ -1,17 +1,13 @@
 const express = require('express');
+const route = require('./routes/client/index.route');
 const app = express();
 const port = 3000
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-  res.render("client/pages/home/index")
-});
-
-app.get('/products',(req, res) => {
-  res.send("<h1>Trang danh sach san pham </h1>")
-});
+//Route
+route(app);
 
 
 app.listen(port, () => {
