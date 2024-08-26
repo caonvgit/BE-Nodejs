0473e5b79b70ebcaccd0f4514062b10c9578de62
[cao nguyen] <[caonguyengithub@gmail.com-email]>
2024-08-26 22:54:03 +0700

    search 2

--- Chap2/controllers/admin/product.controller.js
+++ Chap2/controllers/admin/product.controller.js
@@ -1,49 +1,39 @@
-const Product = require('../../models/products.model')
-   // Get /admin/products
-  module.exports.index = async(req,res)=>{
-    // console.log(req.query.status)
-    let filterStatus = [
-      {
-        name: "Tất cả",
-        status:"",
-        class: "",
-      },
-      {
-        name: "Hoạt động",
-        status:"active",
-        class: "",
+const Product = require("../../models/products.model");
+// Get /admin/products
+module.exports.index = async (req, res) => {
+  // console.log(req.query.status)
+  let filterStatus = [
+    { name: "Tất cả", status: "", class: ""},
+    {name: "Hoạt động",status: "active",class: ""},
+    { name: "Không hoạt động",status: "inactive", class: ""},
+  ];
+  if (req.query.status) {
+    const index = filterStatus.findIndex((item) => item.status == req.query.status);
+    filterStatus[index].class = "active";
+  } else {
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
+  }
 
-      },{
-        name: "Không hoạt động",
-        status:"inactive",
-        class: "",
+  let find = {};
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
 
-      }
-    ];
-    if(req.query.status){
-      const index = filterStatus.findIndex(item => item.status == req.query.status)
-      filterStatus[index].class = "active";
-    }else{
-      const index = filterStatus.findIndex(item => item.status == "")
-      filterStatus[index].class = "active";
-    }
+  let keyword = "";
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword, "i");
+    find.title = regex;
+  }
 
-    let find ={
-      
-    };
-    if(req.query.status){
-      find.status = req.query.status;
-    }   
-    
-    const products = await Product.find(find);
-    // console.log(products);
+  const products = await Product.find(find);
+  // console.log(products);
 
-
-
-    
-    res.render("admin/pages/products/index",{
-      pageTitle : "Danh sách sản phẩm ",
-      products : products,
-      filterStatus: filterStatus
-    })
-    }
+  res.render("admin/pages/products/index", {
+    pageTitle: "Danh sách sản phẩm ",
+    products: products,
+    filterStatus: filterStatus,
+    keyword: keyword,
+  });
+};
--- Chap2/public/admin/js/script.js
+++ Chap2/public/admin/js/script.js
@@ -1,36 +1,35 @@
 // Button status
 const buttonStatus = document.querySelectorAll("[button-status]");
-if(buttonStatus.length > 0) {
-  let url = new URL(window.location.href)
-  buttonStatus.forEach(button =>{
-    button.addEventListener("click",()=>{
+if (buttonStatus.length > 0) {
+  let url = new URL(window.location.href);
+  buttonStatus.forEach((button) => {
+    button.addEventListener("click", () => {
       const status = button.getAttribute("button-status");
-      if(status){
+      if (status) {
         url.searchParams.set("status", status);
-      }else{
+      } else {
         url.searchParams.delete("status");
       }
       // console.log(url);
       window.location.href = url.href;
-    
-    })
-  })
+    });
+  });
 }
- // End Button Status 
+// End Button Status
 
- // Form Search 
- const formSearch = document.querySelector("#form-search");
- if(formSearch){
-    let url = new URL(window.location.href);
-    formSearch.addEventListener("submit", (e) =>{
-      e.preventDefault();
-      const keyword = e.target.elements.keyword.value;
-      if(keyword) {
-        url.searchParams.set("keyword", keyword);
-      }else{
-        url.searchParams.delete("keyword");
-      }
-      window.location.href = url.href;
-    })
- }
- //End Form Search
+// Form Search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
+}
+//End Form Search
