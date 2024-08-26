fc8c530459ec500c46417be2e2556ea366c1f6a2
[cao nguyen] <[caonguyengithub@gmail.com-email]>
2024-08-26 15:51:40 +0700

    loc trang thai

--- Chap2/controllers/admin/product.controller.js
+++ Chap2/controllers/admin/product.controller.js
@@ -2,6 +2,32 @@ const Product = require('../../models/products.model')
    // Get /admin/products
   module.exports.index = async(req,res)=>{
     // console.log(req.query.status)
+    let filterStatus = [
+      {
+        name: "Tất cả",
+        status:"",
+        class: "",
+      },
+      {
+        name: "Hoạt động",
+        status:"active",
+        class: "",
+
+      },{
+        name: "Không hoạt động",
+        status:"inactive",
+        class: "",
+
+      }
+    ];
+    if(req.query.status){
+      const index = filterStatus.findIndex(item => item.status == req.query.status)
+      filterStatus[index].class = "active";
+    }else{
+      const index = filterStatus.findIndex(item => item.status == "")
+      filterStatus[index].class = "active";
+    }
+
     let find ={
       
     };
@@ -13,8 +39,11 @@ const Product = require('../../models/products.model')
     // console.log(products);
 
 
+
+    
     res.render("admin/pages/products/index",{
       pageTitle : "Danh sách sản phẩm ",
-      products : products
+      products : products,
+      filterStatus: filterStatus
     })
     }
