d531b0c4fd83e4360e3c41c13c04d7b808bbd28f
[cao nguyen] <[caonguyengithub@gmail.com-email]>
2024-08-28 16:58:57 +0700

    phan trang pagination

--- Chap2/controllers/admin/product.controller.js
+++ Chap2/controllers/admin/product.controller.js
@@ -1,6 +1,8 @@
 const Product = require("../../models/products.model");
 const filterStatusHelper = require("../../helpers/filterStatus")
 const searchHelper = require("../../helpers/search")
+const paginationHelper = require("../../helpers/pagination")
+
 
 // Get /admin/products
 module.exports.index = async (req, res) => {
@@ -12,14 +14,27 @@ module.exports.index = async (req, res) => {
   if (req.query.status) {
     find.status = req.query.status;
   }
+
   // search
   const objectSearch = searchHelper(req.query);
   if(objectSearch.regex){
     find.title = objectSearch.regex;
   }
 
- 
-  const products = await Product.find(find);
+  //Phân trang
+  const countProducts = await Product.countDocuments(find);
+  let objectPagination = paginationHelper(
+    {
+    currentPage:1,
+    limitItems:4
+  },
+  req.query,
+  countProducts
+  );
+  
+
+  const products = await Product.find(find).limit(objectPagination.limitItems).
+  skip(objectPagination.skip);
   // console.log(products);
 
   res.render("admin/pages/products/index", {
@@ -27,5 +42,6 @@ module.exports.index = async (req, res) => {
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
+    pagination: objectPagination,
   });
 };
